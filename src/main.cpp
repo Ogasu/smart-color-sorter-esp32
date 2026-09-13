f1897d8cd7b0e2b84de4fd69a48d04bb4fdec74f
Ogasu <thanadon19478@gmail.com>
2026-09-13 20:54:29 +0700

    Add LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
-#include <Arduino.h>
-
 // set up the DHT sensor
 #include "DHT.h"
 #define DHTTYPE DHT11   // DHT 11
 const int DHTPIN = 4; // Pin which is connected to the DHT sensor
-DHT dht(DHTPIN, DHTTYPE); 
+DHT dht(DHTPIN, DHTTYPE);  // Initialize DHT sensor for normal 16mhz Arduino
 
 //set up ssd1306 OLED display
 #include <Adafruit_SSD1306.h>
@@ -13,7 +11,17 @@ DHT dht(DHTPIN, DHTTYPE);
 const int SCREEN_WIDTH = 128; // OLED display width, in pixels
 const int SCREEN_HEIGHT = 64; // OLED display height, in pixels
 const int OLED_RESET = -1;    // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Create an instance of the SSD1306 display
+
+//set up led 
+const int LED_RED = 27;
+const int LED_GREEN = 13;
+const int LED_YELLOW = 14;
+
+//set up RGB LED
+const int LED_R = 5;
+const int LED_G = 23;
+const int LED_B = 15; 
 
 void check_equipment();
 
@@ -22,6 +30,14 @@ void setup() {
   dht.begin();
   Wire.begin(21,22); // Initialize I2C with SDA on GPIO 21 and SCL on GPIO 22
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
+
+  // set up the LED pins
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 
   // check the equipment
   check_equipment();
@@ -35,7 +51,14 @@ void setup() {
 }
 
 void loop() {
-  
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.println(t);
+  delay(2000); // Wait a few seconds between measurements.
 }
 
 
@@ -61,5 +84,26 @@ void check_equipment() {
   display.setCursor(0, 0);
   display.println("OLED: OK");
   display.display();
+
+  // check the LEDs
+  digitalWrite(LED_RED, HIGH);
+  delay(500);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(500);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(500);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_R, HIGH);
+  delay(500);
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, HIGH);
+  delay(500);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, HIGH);
+  delay(500);
+  digitalWrite(LED_B, LOW);
+  Serial.println("LEDs: OK(ถ้าไฟติดและดับตามลำดับ)");
 
 }
